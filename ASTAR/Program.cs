4958bc660af209ca7cd847cd9bb357623892e75c
AidanLatno <aidlatno@outlook.com>
2026-06-12 11:57:43 -0700

    First Test program. Looking good

--- ASTAR/Program.cs
+++ ASTAR/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Node[,] grid = new Node[5, 5];
+
+for (int x = 0; x < 5; x++)
+{
+    for (int y = 0; y < 5; y++)
+    {
+        grid[x, y] = new Node(x, y, true);
+    }
+}
+
+grid[2, 2].Walkable = false;
+
+AStar aStar = new AStar(grid);
+
+List<Node> path = aStar.FindPath(grid[0, 0], grid[4, 4]);
+
+if (path != null)
+{
+    foreach (Node node in path)
+    {
+        Console.WriteLine($"({node.X}, {node.Y})");
+    }
+}
+else
+{
+    Console.WriteLine("No path found");
+}
