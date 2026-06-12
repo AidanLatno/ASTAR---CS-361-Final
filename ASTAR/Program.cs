0b428ee67d7518c09857bc0a065ed10346a2e497
AidanLatno <aidlatno@outlook.com>
2026-06-12 12:07:51 -0700

    Added visuals to test program

--- ASTAR/Program.cs
+++ ASTAR/Program.cs
@@ -1,10 +1,26 @@
-﻿Node[,] grid = new Node[5, 5];
+﻿
 
-for (int x = 0; x < 5; x++)
+const int size = 5;
+
+int[,] layoutGrid = new int[size,size]
 {
-    for (int y = 0; y < 5; y++)
+    { 0, 0, 0, 0, 0 },
+    { 0, 1, 1, 1, 0 },
+    { 0, 1, 0, 0, 0 },
+    { 0, 1, 0, 1, 1 },
+    { 0, 1, 0, 0, 0 }
+};
+
+
+
+
+Node[,] grid = new Node[size, size];
+
+for (int x = 0; x < size; x++)
+{
+    for (int y = 0; y < size; y++)
     {
-        grid[x, y] = new Node(x, y, true);
+        grid[x, y] = new Node(x, y, layoutGrid[x, y] == 0);
     }
 }
 
@@ -14,6 +30,7 @@ AStar aStar = new AStar(grid);
 
 List<Node> path = aStar.FindPath(grid[0, 0], grid[4, 4]);
 
+// Print the path
 if (path != null)
 {
     foreach (Node node in path)
@@ -24,4 +41,25 @@ if (path != null)
 else
 {
     Console.WriteLine("No path found");
+}
+
+foreach (Node node in path)
+{
+    layoutGrid[node.X, node.Y] = 2; // Mark the path for visualization
+}
+
+
+// Show path on the grid
+for (int x = 0; x < size; x++)
+{
+    for (int y = 0; y < size; y++)
+    {
+        if(layoutGrid[x, y] == 0)
+            Console.Write(". ");
+        else if(layoutGrid[x, y] == 1)
+            Console.Write("# ");
+        else if(layoutGrid[x, y] == 2)
+            Console.Write("o ");
+    }
+    Console.WriteLine();
 }
